7e62f5924fa07eabb9c06390fa85f27fc2a29c77
Mohammad Hossein Moslemi <mmoslem3@klogin03.paice.vectorinstitute.ai>
2025-12-23 12:36:35 -0500

    add results

--- test_all.sh
+++ test_all.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 set -e
 
-# One log file for everything
-LOG_FILE="test_all_DC_DM.log"
+# # One log file for everything
+# LOG_FILE="test_all_DC_DM.log"
 
-if [ -f "$LOG_FILE" ]; then
-  rm -f "$LOG_FILE"
-fi
+# if [ -f "$LOG_FILE" ]; then
+#   rm -f "$LOG_FILE"
+# fi
 
-touch "$LOG_FILE"
+# touch "$LOG_FILE"
 
 
 # Datasets
 DATASETS=(
-  "Colored_MNIST_foreground"
-  "Colored_MNIST_background"
+  "CIFAR10_S_90"
   "Colored_FashionMNIST_foreground"
   "Colored_FashionMNIST_background"
-  "CIFAR10_S_90"
+  # "Colored_MNIST_foreground"
+  # "Colored_MNIST_background"
+
+  
 )
 
   for dataset in "${DATASETS[@]}"; do
@@ -28,8 +30,7 @@ DATASETS=(
       echo "==================================================" 
 
       python test_DC.py \
-        --dataset "$dataset" \
-        2>&1 | tee -a "$LOG_FILE"
+        --dataset "$dataset" 
 
     #   echo "" 
     #   echo "END: dataset=${dataset}, ipc=${ipc}, metric=${metric}"
